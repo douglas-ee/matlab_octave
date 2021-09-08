281a5c616dab38696b83687929ada66e87583dd4
Douglas S Gomes <douglas.gomes@ee.ufcg.edu.br>
2021-09-08 10:11:33 -0300

    ~..6

--- octave_LCE/Atividade11.m
+++ octave_LCE/Atividade11.m
@@ -1,40 +1,33 @@
+clear
+clc
+
 ## CIRCUITOS PARALELO
 
-% Ir(w) e Ic(w)
-
-
-## organizacao das equacoes necessairas
-%I = Ir + Ic
-%Ir = (1/(j*w*C))/((R + 1)/(j*w*C))
-%V = R*Ir = (1/(j*w*C))*Ic
-%Ir = (1/(j*w*C))/(R*Ic)
-%Ic = R/(1/(j*w*C))Ir
-%I = Ir + Ic
-%I = Ir + R/(1/(j*w*C))Ir
-%(1/(j*w*C))*I = (1/(j*w*C))*Ir + R*Ir
-%((1/(j*w*C)) + R)*Ir = (1/(j*w*C))*I
-%1/(wC) = R
-%1 = wRC
-%w = 1/(RC)
-
-
+% cdados do circuitos
+% corrente
 I = 20e-3;
+% resistencia
 R = 150;
+% capacitancia
 C = 220e-9;
+% amostragem
 w = logspace(0, 5, 1000);
+
 w_c = 1/(R*C);
 
+% equacoes necessarias
 Ir = (1./(j*w*C))./((1./(j*w*C)) + R)*I;
 Ic = (R)./((1./(j*w*C)) + R)*I;
 
 Irc = (1./(j*w_c*C))./((1./(j*w_c*C)) + R)*I;
 Icc = (R)./((1./(j*w_c*C)) + R)*I;
 
-
-
+% plotando o resultado
 figure(1)
 subplot(211), plot(w/(2*pi), abs(Ir), w/(2*pi), abs(Ic), w/(2*pi), abs(Ir+Ic), w_c/(2*pi), abs(Irc), 'o-', w_c/(2*pi), abs(Icc), 'om')
 xlabel('Frequencia [Hz]'), ylabel('|Ir| e |Ic| (A)')
 subplot(212), plot(w/(2*pi), angle(Ir)*180/pi, w/(2*pi), angle(Ic)*180/pi, w/(2*pi), angle(Ir+Ic)*180/pi, w_c/(2*pi), angle(Irc)*180/pi,'o-', w_c/(2*pi), angle(Icc)*180/pi,'om')
 xlabel('Frequencia [Hz]'), ylabel('Ph(Ir) e Ph(Ic) (^o)')
 
+%salvando o grafico em JPG
+saveas(gca, 'grafico_11.jpg', 'jpg')
--- octave_LCE/Atividade12.m
+++ octave_LCE/Atividade12.m
@@ -1,22 +1,32 @@
-## CIRCUITOS SERIE
+clear
 clc
+
+## CIRCUITOS SERIE
+
+% dados do circuitos
+% tensao
 V = 20;
+% resistencia
 R = 220;
+% indutor
 L = 35e-3;
+% amostragem
 w = logspace(0,5,1000);
 wc = R/L;
 fc = wc/(2*pi)
 
+% equacoes necessarias
 Vr = (R)./(j*w*L+R)*V;
 Vl = (j*w*L)./(j*w*L+R)*V;
 
 Vrc = (R)./(j*wc*L+R)*V;
 Vlc = (j*wc*L)./(j*wc*L+R)*V;
 
+% plotando os valores
 subplot(211),plot(w/(2*pi),abs(Vr),w/(2*pi),abs(Vl),wc/(2*pi),abs(Vlc),'o');
 xlabel('Frequencia [Hz]'), ylabel('|VR| e |VL| [V]')
 subplot(212),plot(w/(2*pi),rad2deg(angle(Vr)),w/(2*pi),rad2deg(angle(Vl)),wc/(2*pi),rad2deg(angle(Vrc)),'o',wc/(2*pi),rad2deg(angle(Vlc)),'o');
 xlabel('Frequencia [Hz]'),ylabel('Fase de VR e VL [^o]')
 
 %salvando o grafico em JPG
-saveas(gca, 'grafico_11.jpg', 'jpg')
+saveas(gca, 'grafico_12.jpg', 'jpg')
